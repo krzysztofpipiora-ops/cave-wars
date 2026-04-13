4ec6bf554c1059795bead31428f32cfa22dda0ee
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-04-13 16:24:16 +0200

    Update CaveWars.java

--- src/main/java/pl/stefan/cavewars/CaveWars.java
+++ src/main/java/pl/stefan/cavewars/CaveWars.java
@@ -22,18 +22,17 @@ public class CaveWars extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        getLogger().info("Plugin CaveWars 1.21.4 (Total Ore - No Caves) aktywowany!");
+        getLogger().info("CaveWars 8-osobowe (Mala Arena) aktywowany!");
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (command.getName().equalsIgnoreCase("cwstart")) {
-            if (!sender.isOp()) {
-                sender.sendMessage(ChatColor.RED + "Brak uprawnien!");
-                return true;
-            }
+        if (command.getName().equalsIgnoreCase("cwstart") && sender.isOp()) {
             
-            generateTotalOreArena();
+            // 1. Mala arena dla 8 osob
+            generateSmallArena();
+            
+            // 2. Start (Teleportacja i Szybki Border)
             startMatch();
             
             return true;
@@ -41,13 +40,13 @@ public class CaveWars extends JavaPlugin implements Listener {
         return false;
     }
 
-    private void generateTotalOreArena() {
+    private void generateSmallArena() {
         World world = Bukkit.getWorlds().get(0);
-        int radius = 100; 
+        int radius = 50; // Mniejszy promien idealny dla 8 graczy
         int ceilingY = 40;
         int floorY = -60;
 
-        Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "⛏ TRWA TOTALNE WYPELNIANIE MAPY RUDAMI... Moze to potrwac dluzej!");
+        Bukkit.broadcastMessage(ChatColor.YELLOW + "⛏ Generowanie malej areny (100x100)...");
 
         for (int x = -radius; x <= radius; x++) {
             for (int z = -radius; z <= radius; z++) {
@@ -55,30 +54,21 @@ public class CaveWars extends JavaPlugin implements Listener {
                 // Sufit z bedrocka
                 world.getBlockAt(x, ceilingY, z).setType(Material.BEDROCK);
 
-                // Wypełnianie KAŻDEGO bloku (brak jaskiń, brak wody)
                 for (int y = floorY; y < ceilingY; y++) {
                     Block block = world.getBlockAt(x, y, z);
-                    
                     double chance = random.nextDouble();
 
-                    // 100% bloków pod sufitem staje się rudami:
-                    if (chance < 0.05) { 
-                        block.setType(Material.ANCIENT_DEBRIS); // 5% na Debris
-                    } else if (chance < 0.15) { 
-                        block.setType(Material.DIAMOND_ORE);    // 10% na Diamenty
-                    } else if (chance < 0.35) { 
-                        block.setType(Material.GOLD_ORE);       // 20% na Złoto
-                    } else if (chance < 0.65) { 
-                        block.setType(Material.IRON_ORE);       // 30% na Żelazo
-                    } else if (chance < 0.85) { 
-                        block.setType(Material.COAL_ORE);       // 20% na Węgiel
-                    } else {
-                        block.setType(Material.LAPIS_ORE);      // 15% na Lapis
-                    }
+                    // Wypelnienie surowcami (bez jaskin)
+                    if (chance < 0.05) block.setType(Material.ANCIENT_DEBRIS);
+                    else if (chance < 0.15) block.setType(Material.DIAMOND_ORE);
+                    else if (chance < 0.35) block.setType(Material.GOLD_ORE);
+                    else if (chance < 0.60) block.setType(Material.IRON_ORE);
+                    else if (chance < 0.80) block.setType(Material.COAL_ORE);
+                    else block.setType(Material.LAPIS_ORE);
                 }
             }
         }
-        Bukkit.broadcastMessage(ChatColor.GREEN + "✅ Arena gotowa! Swiat jest litym blokiem surowcow.");
+        Bukkit.broadcastMessage(ChatColor.GREEN + "✅ Mala arena gotowa!");
     }
 
     private void startMatch() {
@@ -86,18 +76,16 @@ public class CaveWars extends JavaPlugin implements Listener {
         WorldBorder border = world.getWorldBorder();
         
         border.setCenter(0, 0);
-        border.setSize(200); 
-        border.setSize(10, 600); 
+        border.setSize(100); // Startowa wielkosc zgodna z arena
+        border.setSize(6, 480); // Kurczenie do 6 kratek w 8 minut (szybsza gra)
 
         for (Player p : Bukkit.getOnlinePlayers()) {
-            int x = random.nextInt(160) - 80;
-            int z = random.nextInt(160) - 80;
-            
-            // Teleportujemy gracza na poziom Y=35 (zaraz pod sufitem), 
-            // bo skoro nie ma jaskiń, muszą zacząć kopać od góry.
+            // Losowanie pozycji wewnatrz malej areny
+            int x = random.nextInt(80) - 40;
+            int z = random.nextInt(80) - 40;
             Location loc = new Location(world, x, 35, z);
             
-            // Czyscimy maly obszar 1x2x1 żeby gracz nie zginal w bloku od razu
+            // Bezpieczny start
             loc.getBlock().setType(Material.AIR);
             loc.clone().add(0, 1, 0).getBlock().setType(Material.AIR);
             
@@ -105,12 +93,13 @@ public class CaveWars extends JavaPlugin implements Listener {
             p.setGameMode(GameMode.SURVIVAL);
             p.getInventory().clear();
             
+            // Wybalansowany zestaw startowy
             p.getInventory().addItem(new ItemStack(Material.IRON_PICKAXE));
-            p.getInventory().addItem(new ItemStack(Material.BREAD, 64));
+            p.getInventory().addItem(new ItemStack(Material.BREAD, 32));
             p.getInventory().addItem(new ItemStack(Material.CRAFTING_TABLE));
-            p.getInventory().addItem(new ItemStack(Material.TORCH, 64));
+            p.getInventory().addItem(new ItemStack(Material.TORCH, 32));
             
-            p.sendMessage(ChatColor.RED + "Kop w dol! Caly swiat to rudy!");
+            p.sendMessage(ChatColor.RED + "Mala arena! Walka zaraz sie zacznie!");
             p.playSound(p.getLocation(), Sound.EVENT_RAID_HORN, 1.0f, 1.0f);
         }
     }
@@ -118,27 +107,21 @@ public class CaveWars extends JavaPlugin implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Block b = event.getBlock();
-        Player p = event.getPlayer();
-
         if (b.getType() == Material.IRON_ORE || b.getType() == Material.DEEPSLATE_IRON_ORE) {
             event.setDropItems(false);
             b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.IRON_INGOT));
         }
-        
         if (b.getType() == Material.GOLD_ORE || b.getType() == Material.DEEPSLATE_GOLD_ORE) {
             event.setDropItems(false);
             b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GOLD_INGOT));
         }
-
         if (b.getType() == Material.ANCIENT_DEBRIS) {
-            p.sendMessage(ChatColor.LIGHT_PURPLE + "⛏ Debris!");
-            p.playSound(p.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_CHIME, 1.0f, 1.0f);
+            event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_AMETHYST_BLOCK_CHIME, 1.0f, 1.0f);
         }
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         event.getEntity().setGameMode(GameMode.SPECTATOR);
-        Bukkit.broadcastMessage(ChatColor.RED + "☠ " + event.getEntity().getName() + " odpadl!");
     }
 }
